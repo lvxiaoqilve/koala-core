c11281f954ad61e04b94b75e9e03034683210997
杨文生 <190332447@qq.com>
2017-06-02 13:44:59 +0800

    修改pom

--- koala-base/src/main/java/com/xingling/pojo/BaseEntiy.java
+++ koala-base/src/main/java/com/xingling/pojo/BaseEntiy.java
@@ -1,7 +1,12 @@
 package com.xingling.pojo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.sql.Timestamp;
 
 @Data
@@ -10,25 +15,39 @@ public class BaseEntiy {
     /**
      * 主键 id
      */
+    @Id
+    @GeneratedValue(generator = "UUID")
     protected Long id;
     /**
      * 创建者id
      */
+    @Column(name = "creator")
     protected Long creator;
     /**
      * 修改者id
      */
+    @Column(name = "updater")
     protected Long updater;
+
     /**
      * 创建时间
      */
+    @Column(name = "updater")
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     protected Timestamp createtime;
+
     /**
      * 更改时间
      */
+    @Column(name = "updater")
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     protected Timestamp updatetime;
+
     /**
      *删除标识
      */
+    @Column(name = "del")
     protected int del;
 }
