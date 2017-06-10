9a1d7ec96be5443638f0e5461d51f0f7ee90768f
杨文生 <190332447@qq.com>
2017-06-10 15:45:20 +0800

    修改BaseEntity

--- koala-base/src/main/java/com/xingling/pojo/BaseEntiy.java
+++ koala-base/src/main/java/com/xingling/pojo/BaseEntiy.java
@@ -7,47 +7,48 @@ import org.springframework.format.annotation.DateTimeFormat;
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import java.sql.Timestamp;
+import java.util.Date;
 
 @Data
 public class BaseEntiy {
 
-    /**
-     * 主键 id
-     */
-    @Id
-    @GeneratedValue(generator = "UUID")
-    protected Long id;
-    /**
-     * 创建者id
-     */
-    @Column(name = "creator")
-    protected Long creator;
-    /**
-     * 修改者id
-     */
-    @Column(name = "updater")
-    protected Long updater;
 
-    /**
-     * 创建时间
-     */
-    @Column(name = "updater")
-    @DateTimeFormat(pattern="yyyy-MM-dd")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-    protected Timestamp createtime;
+	/**
+	 * 主键 id
+	 */
+	@Id
+	@GeneratedValue(generator = "UUID")
+	private String id;
+	/**
+	 * 创建者id
+	 */
+	@Column(name = "creator")
+	private Long creator;
+	/**
+	 * 修改者id
+	 */
+	@Column(name = "updater")
+	private Long updater;
 
-    /**
-     * 更改时间
-     */
-    @Column(name = "updater")
-    @DateTimeFormat(pattern="yyyy-MM-dd")
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-    protected Timestamp updatetime;
+	/**
+	 * 创建时间
+	 */
+	@Column(name = "create_time")
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+	private Date createTime;
 
-    /**
-     *删除标识
-     */
-    @Column(name = "del")
-    protected int del;
+	/**
+	 * 更改时间
+	 */
+	@Column(name = "update_time")
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+	private Date updateTime;
+
+	/**
+	 *删除标识
+	 */
+	@Column(name = "del")
+	private Integer del;
 }
